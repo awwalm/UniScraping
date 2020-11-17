d1bc7dc05ea97ce863810fdadb7de30f80d75a75
M-Awwal <67423428+M-Awwal@users.noreply.github.com>
2020-11-17 18:10:44 +0800

    Add files via upload

---
+++ PLTCollegeOfLaw/PLT_COL_Data.py
@@ -0,0 +1,357 @@
+import copy
+import csv
+import json
+import os
+import re
+import sre_constants
+import time
+from datetime import date as dt
+from pathlib import Path
+from urllib.parse import urljoin
+
+import bs4 as bs4
+import requests
+# noinspection PyProtectedMembe
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, ElementNotInteractableException, NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from CustomMethods import TemplateData
+from CustomMethods.DurationConverter import convert_duration, convert_duration_cleanly
+
+
+def get_page(_url_):
+    """Will download the contents of the page using the requests library.
+    :return: a BeautifulSoup object i.e. the content of the webpage related to the given URL.
+    """
+    # noinspection PyBroadException,PyUnusedLocal
+    try:
+        r = requests.get(_url_)
+        if r.status_code == 200:
+            return bs4.BeautifulSoup(r.content, 'html.parser')
+    except Exception:
+        pass
+    return None
+
+
+def webdriver_wait(_xpath_):
+    try:
+        WebDriverWait(browser, delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f"{_xpath_}"))
+        )
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException) as e:
+        print(e.traceback)
+
+
+def remove_banned_words(to_print, database_regex):
+    _pattern_ = re.compile(r"\b(" + "|".join(database_regex) + ")\\W", re.I)
+    return _pattern_.sub("", to_print)
+
+
+def save_data_json(title__, data__):
+    with open(title__, 'w', encoding='utf-8') as f:
+        json.dump(data__, f, ensure_ascii=False, indent=2)
+
+
+def tag_text(_tag_):
+    return _tag_.get_text().__str__().strip()
+
+
+def has_numbers(_input_string_):
+    return any(char.isdigit() for char in _input_string_)
+
+
+# selenium web driver
+# we need the Chrome driver to simulate JavaScript functionality
+# thus, we set the executable path and driver options arguments
+# ENSURE YOU CHANGE THE DIRECTORY AND EXE PATH IF NEEDED (UNLESS YOU'RE NOT USING WINDOWS!)
+option = webdriver.ChromeOptions()
+option.add_argument(" - incognito")
+# option.add_argument("headless")
+exec_path = Path(os.getcwd().replace('\\', '/'))
+exec_path = exec_path.parent.parent.parent.__str__() + '/Libraries/Google/v86/chromedriver.exe'
+browser = webdriver.Chrome(executable_path=exec_path, chrome_options=option)
+browser.maximize_window()
+delay = 1
+
+# collect page source for fees
+"""
+with open('LocalData/excelsia-tuition-fees.html', encoding='utf8') as dom_file:  # Domestic Fees local HTML
+    soup_fees = bs4.BeautifulSoup(dom_file, 'html.parser')
+option2 = webdriver.ChromeOptions()
+option2.add_argument(" - incognito")
+option2.add_argument("headless")
+browser2 = webdriver.Chrome(executable_path=exec_path, chrome_options=option2)
+browser2.get("file:///"+os.getcwd()+"/LocalData/excelsia-tuition-fees.html")
+time.sleep(1)
+fee_webpage = browser2.page_source
+"""
+
+# read the url from each file into a list
+course_links_file_path = Path(os.getcwd().replace('\\', '/'))
+course_links_file_path = course_links_file_path.__str__() + '/plt_col_links_file'
+course_links_file = open(course_links_file_path, 'r')
+
+# the csv file we'll be saving the courses to
+csv_file_path = Path(os.getcwd().replace('\\', '/'))
+csv_file = 'PLT_COL_All_Data.csv'
+
+level_key = TemplateData.level_key  # dictionary of course levels
+
+faculty_key = TemplateData.faculty_key  # dictionary of course levels
+
+currency_pattern = "(?:[\£\$\€\(RM)\]{1}[,\d]+.?\d*)"  # regex pattern for finding currency or cash amount strings
+
+course_data_all = []
+
+city_set = set()
+
+bar = ' | '  # generic text separator for tidiness
+
+months = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6,
+          'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
+
+course_data_template = {'Level_Code': '', 'University': '', 'City': '', 'Course': '',
+                        'Faculty': '',
+                        'Int_Fees': '', 'Local_Fees': '', 'Currency': 'AUD', 'Currency_Time': 'Years', 'Duration': '',
+                        'Duration_Time': '', 'Full_Time': 'Yes', 'Part_Time': 'No',
+                        'Prerequisite_1': '', 'Prerequisite_1_grade_1': '',  # ATAR
+                        'Prerequisite_2': '', 'Prerequisite_2_grade_2': '',  # IELTS
+                        'Prerequisite_3': '', 'Prerequisite_3_grade_3': '',  # GPA
+                        'Website': '', 'Course_Lang': 'English', 'Availability': 'A', 'Description': '',
+                        'Career_Outcomes': '',
+                        'Country': 'Australia', 'Online': 'No', 'Offline': 'Yes', 'Distance': 'No',
+                        'Face_to_Face': 'Yes',
+                        'Blended': 'No', 'Remarks': ''}
+
+# noinspection SpellCheckingInspection
+possible_cities = {'ACT': 'Australian Capital Territory',
+                   'NSW': 'New South Wales',
+                   'NT': 'Norther Territory',
+                   'QLD': 'Queensland',
+                   'SA': 'South Australia',
+                   'VIC': 'Victoria',
+                   'WA': 'Western Australia'}
+
+other_cities = {}
+
+sample = [""]
+
+# MAIN ROUTINE
+for each_url in course_links_file:
+
+    course_data = {'Level_Code': '', 'University': 'The College of Law', 'City': '', 'Course': '',
+                   'Faculty': '',
+                   'Int_Fees': '', 'Local_Fees': '', 'Currency': 'AUD', 'Currency_Time': 'Course', 'Duration': '',
+                   'Duration_Time': '', 'Full_Time': 'Yes', 'Part_Time': 'No',
+                   'Prerequisite_1': '', 'Prerequisite_1_grade_1': '',  # ATAR
+                   'Prerequisite_2': '', 'Prerequisite_2_grade_2': '',  # IELTS or anything else
+                   'Prerequisite_3': '', 'Prerequisite_3_grade_3': '',  # GPA or IB
+                   'Website': '', 'Course_Lang': 'English', 'Availability': 'A', 'Description': '',
+                   'Career_Outcomes': '',
+                   'Country': 'Australia', 'Online': 'No', 'Offline': 'Yes', 'Distance': 'No', 'Face_to_Face': 'Yes',
+                   'Blended': 'No', 'Remarks': ''}
+
+    actual_cities = set()
+
+    browser.get(each_url)
+    # time.sleep(1)
+    url__ = each_url
+    pure_url = each_url.strip()
+    print('CURRENT LINK: ', pure_url)
+    each_url = browser.page_source
+    soup = bs4.BeautifulSoup(each_url, 'lxml')
+
+    all_text = soup.text.replace('\n', '').strip()
+
+    # COURSE URL
+    course_data['Website'] = pure_url
+
+    # COURSE NAME
+    title_ = None
+    title = soup.find('h1', {'class': 'course-title'})
+    if title:
+        course_data['Course'] = tag_text(title)
+        title_ = tag_text(title)
+
+    # DECIDE THE LEVEL CODE
+    lev_str = str()
+    for i in level_key:
+        for j in level_key[i]:
+            if j in course_data['Course']:
+                course_data['Level_Code'] = i
+
+    # DECIDE THE FACULTY
+    fac_str = str()
+    for i in faculty_key:
+        for j in faculty_key[i]:
+            if j.lower() in course_data['Course'].lower():
+                course_data['Faculty'] = i
+                fac_str = i
+
+    # DESCRIPTION
+    try:
+        desc_tag = soup.find('div', {'class': 'course-description'}).find_all('p')
+        if desc_tag:
+            course_data['Description'] = tag_text(desc_tag)
+    except AttributeError:
+        pass
+
+    # DELIVERY MODE
+    try:
+        THE_XPATH = "//ul[@class='course-tags']"
+        WebDriverWait(browser, delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f'{THE_XPATH}'))
+        )
+        value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+        if 'part-time' in value.lower() or 'part time' in value.lower():
+            course_data['Part_Time'] = 'Yes'
+        if 'full-time' in value.lower() or 'full time' in value.lower():
+            course_data['Full_Time'] = 'Yes'
+        if 'online' in value.lower():
+            course_data['Online'] = 'Yes'
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException):
+        print('cant extract delivery mode tags')
+
+    # FEES
+    try:
+        THE_XPATH = "//span[@class='price-label' and contains(text(), 'Domestic')]/following::*[1]"
+        WebDriverWait(browser, delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f'{THE_XPATH}'))
+        )
+        value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+        course_data['Local_Fees'] = value.replace('$', '').replace(',', '')
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException):
+        print('cant extract local fees')
+    try:
+        THE_XPATH = "//span[@class='price-label' and contains(text(), 'International')]/following::*[1]"
+        WebDriverWait(browser, delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f'{THE_XPATH}'))
+        )
+        value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+        course_data['Int_Fees'] = value.replace('$', '').replace(',', '')
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException):
+        print('cant extract int fees')
+
+    # OUTCOMES
+    try:
+        THE_XPATH = "//*[contains(text(), 'Overview')]/ancestor::div[1]/following::*[1]"
+        WebDriverWait(browser, delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f'{THE_XPATH}'))
+        )
+        value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+        course_data['Career_Outcomes'] = value
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException):
+        print('cant extract outcomes')
+
+    # REMARKS
+    try:
+        THE_XPATH = "//*[contains(text(), 'Structure')]/ancestor::div[1]/following::*[1]"
+        WebDriverWait(browser, delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f'{THE_XPATH}'))
+        )
+        value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+        course_data['Remarks'] = value
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException):
+        print('cant extract remarks')
+    try:
+        THE_XPATH = "//p[@class='label course-id']"
+        WebDriverWait(browser, delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f'{THE_XPATH}'))
+        )
+        value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+        course_data['Remarks'] = value + bar + course_data['Remarks']
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException):
+        print('cant extract course code')
+
+    # DURATION
+    start_date, end_date = None, None
+    try:
+        THE_XPATH = "//*[contains(text(), 'Start date')]/following::*[1]"
+        WebDriverWait(browser, delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f'{THE_XPATH}'))
+        )
+        value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+        x = value.split()
+        start_date = dt(int(x[-1]), months[x[1]], int(x[0]))
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException):
+        print('cant extract start date')
+    try:
+        THE_XPATH = "//*[contains(text(), 'End date')]/following::*[1]"
+        WebDriverWait(browser, delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f'{THE_XPATH}'))
+        )
+        value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+        x = value.split()
+        end_date = dt(int(x[-1]), months[x[1]], int(x[0]))
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException):
+        print('cant extract end date')
+    if end_date is not None and start_date is not None:
+        duration = str((end_date - start_date).days) + " days"
+        course_data = convert_duration_cleanly(tagged_text=duration, course_dict=course_data)
+        # print(duration)
+
+    # LOCATION
+    for i in possible_cities:
+        if i in course_data['Course']:
+            actual_cities.add(i)
+
+    # duplicating entries with multiple cities for each city
+    cities_covered = []
+    for i in actual_cities:
+        if possible_cities[i] in cities_covered:
+            continue
+        course_data['City'] = possible_cities[i]
+        print('repeated cities: ', possible_cities[i])
+        course_data_all.append(copy.deepcopy(course_data))
+        cities_covered.append(possible_cities[i])
+
+    del actual_cities, cities_covered
+
+    print('COURSE: ', course_data['Course'])
+    print('DURATION + DURATION TIME: ', course_data['Duration'], course_data['Duration_Time'])
+    print('DESCRIPTION: ', course_data['Description'])
+    print('LEVEL CODE: ', course_data['Level_Code'])
+    print('CITY: ', course_data['City'])
+    print('AVAILABILITY: ', course_data['Availability'])
+    print('FACULTY: ', course_data['Faculty'])
+    print('INT FEES: ', course_data['Int_Fees'])
+    print('LOCAL FEES: ', course_data['Local_Fees'])
+    print('ATAR: ', course_data['Prerequisite_1_grade_1'])
+    # print(': ', course_data['Prerequisite_2_grade_2'])
+    # print(': ', course_data['Prerequisite_3_grade_3'])
+    print('FULL TIME: ', course_data['Full_Time'])
+    print('PART-TIME: ', course_data['Part_Time'])
+    print('DISTANCE: ', course_data['Distance'])
+    print('BLENDED: ', course_data['Blended'])
+    print('OFFLINE: ', course_data['Offline'])
+    print('ONLINE: ', course_data['Online'])
+    print('FACE TO FACE: ', course_data['Face_to_Face'])
+    print('OUTCOMES: ', course_data['Career_Outcomes'])
+    print('REMARKS: ', course_data['Remarks'])
+    print()
+
+print(*course_data_all, sep='\n')
+
+# tabulate our data__
+# course_dict_keys = set().union(*(d.keys() for d in course_data_all))
+course_dict_keys = []
+for i in course_data_template:
+    course_dict_keys.append(i)
+
+with open(csv_file, 'w', encoding='utf-8', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, course_dict_keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(course_data_all)
+
+browser.quit()
