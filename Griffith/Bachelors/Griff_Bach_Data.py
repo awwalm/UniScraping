66152ecafce65450e8d8468ac891284d829e8cf6
M-Awwal <67423428+M-Awwal@users.noreply.github.com>
2020-12-01 19:44:15 +0800

    Add files via upload

--- Griffith/Bachelors/Griff_Bach_Data.py
+++ Griffith/Bachelors/Griff_Bach_Data.py
@@ -6,8 +6,14 @@ import time
 from pathlib import Path
 
 # noinspection PyProtectedMember
+from urllib.parse import urljoin
+
 from bs4 import Comment
 from selenium import webdriver
+from selenium.common.exceptions import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from CustomMethods import DurationConverter, TemplateData
 import bs4 as bs4
 import requests
@@ -67,7 +73,7 @@ def has_numbers(input_string):
 # ENSURE YOU CHANGE THE DIRECTORY AND EXE PATH IF NEEDED (UNLESS YOU'RE NOT USING WINDOWS!)
 option = webdriver.ChromeOptions()
 option.add_argument(" - incognito")
-option.add_argument("headless")
+# option.add_argument("headless")
 exec_path = Path(os.getcwd().replace('\\', '/'))
 exec_path = exec_path.parent.parent.parent.parent.__str__() + '/Libraries/Google/v86/chromedriver.exe'
 browser = webdriver.Chrome(executable_path=exec_path, chrome_options=option)
@@ -100,7 +106,47 @@ course_data_template = {'Level_Code': '', 'University': 'Griffith University', '
                         'Career_Outcomes': '',
                         'Country': 'Australia', 'Online': 'No', 'Offline': 'Yes', 'Distance': 'No',
                         'Face_to_Face': 'Yes',
-                        'Blended': 'No', 'Remarks': ''}
+                        'Blended': 'No', 'Remarks': '',
+                        'Subject_or_Unit_1': '', 'Subject_Objective_1': '', 'Subject_Description_1': '',
+                        'Subject_or_Unit_2': '', 'Subject_Objective_2': '', 'Subject_Description_2': '',
+                        'Subject_or_Unit_3': '', 'Subject_Objective_3': '', 'Subject_Description_3': '',
+                        'Subject_or_Unit_4': '', 'Subject_Objective_4': '', 'Subject_Description_4': '',
+                        'Subject_or_Unit_5': '', 'Subject_Objective_5': '', 'Subject_Description_5': '',
+                        'Subject_or_Unit_6': '', 'Subject_Objective_6': '', 'Subject_Description_6': '',
+                        'Subject_or_Unit_7': '', 'Subject_Objective_7': '', 'Subject_Description_7': '',
+                        'Subject_or_Unit_8': '', 'Subject_Objective_8': '', 'Subject_Description_8': '',
+                        'Subject_or_Unit_9': '', 'Subject_Objective_9': '', 'Subject_Description_9': '',
+                        'Subject_or_Unit_10': '', 'Subject_Objective_10': '', 'Subject_Description_10': '',
+                        'Subject_or_Unit_11': '', 'Subject_Objective_11': '', 'Subject_Description_11': '',
+                        'Subject_or_Unit_12': '', 'Subject_Objective_12': '', 'Subject_Description_12': '',
+                        'Subject_or_Unit_13': '', 'Subject_Objective_13': '', 'Subject_Description_13': '',
+                        'Subject_or_Unit_14': '', 'Subject_Objective_14': '', 'Subject_Description_14': '',
+                        'Subject_or_Unit_15': '', 'Subject_Objective_15': '', 'Subject_Description_15': '',
+                        'Subject_or_Unit_16': '', 'Subject_Objective_16': '', 'Subject_Description_16': '',
+                        'Subject_or_Unit_17': '', 'Subject_Objective_17': '', 'Subject_Description_17': '',
+                        'Subject_or_Unit_18': '', 'Subject_Objective_18': '', 'Subject_Description_18': '',
+                        'Subject_or_Unit_19': '', 'Subject_Objective_19': '', 'Subject_Description_19': '',
+                        'Subject_or_Unit_20': '', 'Subject_Objective_20': '', 'Subject_Description_20': '',
+                        'Subject_or_Unit_21': '', 'Subject_Objective_21': '', 'Subject_Description_21': '',
+                        'Subject_or_Unit_22': '', 'Subject_Objective_22': '', 'Subject_Description_22': '',
+                        'Subject_or_Unit_23': '', 'Subject_Objective_23': '', 'Subject_Description_23': '',
+                        'Subject_or_Unit_24': '', 'Subject_Objective_24': '', 'Subject_Description_24': '',
+                        'Subject_or_Unit_25': '', 'Subject_Objective_25': '', 'Subject_Description_25': '',
+                        'Subject_or_Unit_26': '', 'Subject_Objective_26': '', 'Subject_Description_26': '',
+                        'Subject_or_Unit_27': '', 'Subject_Objective_27': '', 'Subject_Description_27': '',
+                        'Subject_or_Unit_28': '', 'Subject_Objective_28': '', 'Subject_Description_28': '',
+                        'Subject_or_Unit_29': '', 'Subject_Objective_29': '', 'Subject_Description_29': '',
+                        'Subject_or_Unit_30': '', 'Subject_Objective_30': '', 'Subject_Description_30': '',
+                        'Subject_or_Unit_31': '', 'Subject_Objective_31': '', 'Subject_Description_31': '',
+                        'Subject_or_Unit_32': '', 'Subject_Objective_32': '', 'Subject_Description_32': '',
+                        'Subject_or_Unit_33': '', 'Subject_Objective_33': '', 'Subject_Description_33': '',
+                        'Subject_or_Unit_34': '', 'Subject_Objective_34': '', 'Subject_Description_34': '',
+                        'Subject_or_Unit_35': '', 'Subject_Objective_35': '', 'Subject_Description_35': '',
+                        'Subject_or_Unit_36': '', 'Subject_Objective_36': '', 'Subject_Description_36': '',
+                        'Subject_or_Unit_37': '', 'Subject_Objective_37': '', 'Subject_Description_37': '',
+                        'Subject_or_Unit_38': '', 'Subject_Objective_38': '', 'Subject_Description_38': '',
+                        'Subject_or_Unit_39': '', 'Subject_Objective_39': '', 'Subject_Description_39': '',
+                        'Subject_or_Unit_40': '', 'Subject_Objective_40': '', 'Subject_Description_40': ''}
 
 possible_cities = {'Gold Coast': 'Brisbane',  # > Southport > Gold Coast > Brisbane > Queensland
                    'Logan': 'Brisbane',  # > Brisbane > Queensland
@@ -129,7 +175,47 @@ for each_url in course_links_file:
                    'Website': '', 'Course_Lang': 'English', 'Availability': 'A', 'Description': '',
                    'Career_Outcomes': '',
                    'Country': 'Australia', 'Online': 'No', 'Offline': 'Yes', 'Distance': 'No', 'Face_to_Face': 'Yes',
-                   'Blended': 'No', 'Remarks': ''}
+                   'Blended': 'No', 'Remarks': '',
+                   'Subject_or_Unit_1': '', 'Subject_Objective_1': '', 'Subject_Description_1': '',
+                   'Subject_or_Unit_2': '', 'Subject_Objective_2': '', 'Subject_Description_2': '',
+                   'Subject_or_Unit_3': '', 'Subject_Objective_3': '', 'Subject_Description_3': '',
+                   'Subject_or_Unit_4': '', 'Subject_Objective_4': '', 'Subject_Description_4': '',
+                   'Subject_or_Unit_5': '', 'Subject_Objective_5': '', 'Subject_Description_5': '',
+                   'Subject_or_Unit_6': '', 'Subject_Objective_6': '', 'Subject_Description_6': '',
+                   'Subject_or_Unit_7': '', 'Subject_Objective_7': '', 'Subject_Description_7': '',
+                   'Subject_or_Unit_8': '', 'Subject_Objective_8': '', 'Subject_Description_8': '',
+                   'Subject_or_Unit_9': '', 'Subject_Objective_9': '', 'Subject_Description_9': '',
+                   'Subject_or_Unit_10': '', 'Subject_Objective_10': '', 'Subject_Description_10': '',
+                   'Subject_or_Unit_11': '', 'Subject_Objective_11': '', 'Subject_Description_11': '',
+                   'Subject_or_Unit_12': '', 'Subject_Objective_12': '', 'Subject_Description_12': '',
+                   'Subject_or_Unit_13': '', 'Subject_Objective_13': '', 'Subject_Description_13': '',
+                   'Subject_or_Unit_14': '', 'Subject_Objective_14': '', 'Subject_Description_14': '',
+                   'Subject_or_Unit_15': '', 'Subject_Objective_15': '', 'Subject_Description_15': '',
+                   'Subject_or_Unit_16': '', 'Subject_Objective_16': '', 'Subject_Description_16': '',
+                   'Subject_or_Unit_17': '', 'Subject_Objective_17': '', 'Subject_Description_17': '',
+                   'Subject_or_Unit_18': '', 'Subject_Objective_18': '', 'Subject_Description_18': '',
+                   'Subject_or_Unit_19': '', 'Subject_Objective_19': '', 'Subject_Description_19': '',
+                   'Subject_or_Unit_20': '', 'Subject_Objective_20': '', 'Subject_Description_20': '',
+                   'Subject_or_Unit_21': '', 'Subject_Objective_21': '', 'Subject_Description_21': '',
+                   'Subject_or_Unit_22': '', 'Subject_Objective_22': '', 'Subject_Description_22': '',
+                   'Subject_or_Unit_23': '', 'Subject_Objective_23': '', 'Subject_Description_23': '',
+                   'Subject_or_Unit_24': '', 'Subject_Objective_24': '', 'Subject_Description_24': '',
+                   'Subject_or_Unit_25': '', 'Subject_Objective_25': '', 'Subject_Description_25': '',
+                   'Subject_or_Unit_26': '', 'Subject_Objective_26': '', 'Subject_Description_26': '',
+                   'Subject_or_Unit_27': '', 'Subject_Objective_27': '', 'Subject_Description_27': '',
+                   'Subject_or_Unit_28': '', 'Subject_Objective_28': '', 'Subject_Description_28': '',
+                   'Subject_or_Unit_29': '', 'Subject_Objective_29': '', 'Subject_Description_29': '',
+                   'Subject_or_Unit_30': '', 'Subject_Objective_30': '', 'Subject_Description_30': '',
+                   'Subject_or_Unit_31': '', 'Subject_Objective_31': '', 'Subject_Description_31': '',
+                   'Subject_or_Unit_32': '', 'Subject_Objective_32': '', 'Subject_Description_32': '',
+                   'Subject_or_Unit_33': '', 'Subject_Objective_33': '', 'Subject_Description_33': '',
+                   'Subject_or_Unit_34': '', 'Subject_Objective_34': '', 'Subject_Description_34': '',
+                   'Subject_or_Unit_35': '', 'Subject_Objective_35': '', 'Subject_Description_35': '',
+                   'Subject_or_Unit_36': '', 'Subject_Objective_36': '', 'Subject_Description_36': '',
+                   'Subject_or_Unit_37': '', 'Subject_Objective_37': '', 'Subject_Description_37': '',
+                   'Subject_or_Unit_38': '', 'Subject_Objective_38': '', 'Subject_Description_38': '',
+                   'Subject_or_Unit_39': '', 'Subject_Objective_39': '', 'Subject_Description_39': '',
+                   'Subject_or_Unit_40': '', 'Subject_Objective_40': '', 'Subject_Description_40': ''}
 
     actual_cities = []
 
@@ -148,9 +234,9 @@ for each_url in course_links_file:
     course_data['Website'] = pure_url
 
     # COURSE NAME
-    div1 = soup.find('div', class_='banner study feature').find('div', class_='flex')
+    div1 = soup.find('div', class_='banner study feature')
     if div1:
-        course = tag_text(div1)
+        course = tag_text(div1).replace('\n', '')
         course_data['Course'] = course
     print('COURSE NAME: ', course_data['Course'])
 
@@ -159,14 +245,17 @@ for each_url in course_links_file:
     print('AVAILABILITY: ', course_data['Availability'])
 
     # COURSE DESCRIPTION
-    div1 = soup.find('div', class_='degree-content', itemprop=re.compile('description')).find('div')
-    if div1:
-        p_tags = div1.find_all('p')
-        if p_tags:
-            descriptions = [tag_text(p) for p in p_tags]
-            description = ' '.join(descriptions)
-            course_data['Description'] = description
-    print('DESCRIPTION: ', course_data['Description'])
+    try:
+        div1 = soup.find('div', class_='degree-content', itemprop=re.compile('description')).find('div')
+        if div1:
+            p_tags = div1.find_all('p')
+            if p_tags:
+                descriptions = [tag_text(p) for p in p_tags]
+                description = ' '.join(descriptions)
+                course_data['Description'] = description
+        print('DESCRIPTION: ', course_data['Description'])
+    except AttributeError:
+        pass
 
     # DECIDE THE LEVEL CODE
     for i in level_key:
@@ -217,40 +306,43 @@ for each_url in course_links_file:
                 course_data['Prerequisite_2_grade_2'] = ielts_temp_1
                 course_data['Prerequisite_2'] = 'IELTS'
     except AttributeError:  # get the GPA instead
-        span_p = soup.find('span', itemprop=re.compile('coursePrerequisites')).find('p')
-        if 'GPA' in tag_text(span_p):
-            print('No IELTS value, switching to GPA...')
-            gpa_temp = tag_text(span_p)
-            g = gpa_temp
-            if '4.' in g or '5.' in g or '6.' in g or '7.' or '8.' in g or '9.' in g \
-                    and '4/' not in g and '6/' not in g and '7/' not in g and '8/' not in g:
-                gpa_temp_1 = int(list(filter(str.isdigit, g))[0])
-                gpa_temp_2 = int(list(filter(str.isdigit, g))[1])
-                gpa_temp_val = str(gpa_temp_1) + '.' + str(gpa_temp_2)
-                course_data['Prerequisite_3_grade_3'] = gpa_temp_val
-                course_data['Prerequisite_3'] = 'GPA'
-                if 'Any 2 OUA' in g:
-                    gpa_temp_1 = int(list(filter(str.isdigit, g))[1])
-                    gpa_temp_2 = int(list(filter(str.isdigit, g))[2])
+        try:
+            span_p = soup.find('span', itemprop=re.compile('coursePrerequisites')).find('p')
+            if 'GPA' in tag_text(span_p):
+                print('No IELTS value, switching to GPA...')
+                gpa_temp = tag_text(span_p)
+                g = gpa_temp
+                if '4.' in g or '5.' in g or '6.' in g or '7.' or '8.' in g or '9.' in g \
+                        and '4/' not in g and '6/' not in g and '7/' not in g and '8/' not in g:
+                    gpa_temp_1 = int(list(filter(str.isdigit, g))[0])
+                    gpa_temp_2 = int(list(filter(str.isdigit, g))[1])
                     gpa_temp_val = str(gpa_temp_1) + '.' + str(gpa_temp_2)
                     course_data['Prerequisite_3_grade_3'] = gpa_temp_val
                     course_data['Prerequisite_3'] = 'GPA'
-            elif '4' in g or '5' in g or '6' in g or '7' in g or '8' in g:
-                gpa_temp_1 = int(list(filter(str.isdigit, g))[0])
-                course_data['Prerequisite_3_grade_3'] = gpa_temp_1
-                course_data['Prerequisite_3'] = 'GPA'
-                if 'Any 2 OUA' in g:
-                    gpa_temp_1 = int(list(filter(str.isdigit, g))[1])
-                    gpa_temp_val = str(gpa_temp_1)
-                    course_data['Prerequisite_3_grade_3'] = gpa_temp_val
+                    if 'Any 2 OUA' in g:
+                        gpa_temp_1 = int(list(filter(str.isdigit, g))[1])
+                        gpa_temp_2 = int(list(filter(str.isdigit, g))[2])
+                        gpa_temp_val = str(gpa_temp_1) + '.' + str(gpa_temp_2)
+                        course_data['Prerequisite_3_grade_3'] = gpa_temp_val
+                        course_data['Prerequisite_3'] = 'GPA'
+                elif '4' in g or '5' in g or '6' in g or '7' in g or '8' in g:
+                    gpa_temp_1 = int(list(filter(str.isdigit, g))[0])
+                    course_data['Prerequisite_3_grade_3'] = gpa_temp_1
                     course_data['Prerequisite_3'] = 'GPA'
+                    if 'Any 2 OUA' in g:
+                        gpa_temp_1 = int(list(filter(str.isdigit, g))[1])
+                        gpa_temp_val = str(gpa_temp_1)
+                        course_data['Prerequisite_3_grade_3'] = gpa_temp_val
+                        course_data['Prerequisite_3'] = 'GPA'
 
-            print(tag_text(span_p))
-        else:
-            course_data['Prerequisite_2'] = ''
-            course_data['Prerequisite_2_grade_2'] = ''
-            course_data['Prerequisite_3'] = ''
-            course_data['Prerequisite_3_grade_3'] = ''
+                print(tag_text(span_p))
+            else:
+                course_data['Prerequisite_2'] = ''
+                course_data['Prerequisite_2_grade_2'] = ''
+                course_data['Prerequisite_3'] = ''
+                course_data['Prerequisite_3_grade_3'] = ''
+        except (AttributeError, TypeError, IndexError):
+            pass
 
     # CITY (actually campus), DISTANCE LEARNING, ONLINE LEARNING
     dt1 = soup.find('dt', class_='info-group-title__ campus', text=re.compile('Campus', re.IGNORECASE))
@@ -344,6 +436,63 @@ for each_url in course_links_file:
             and course_data['Part_Time'] == 'Yes' and course_data['Full_Time'] == 'Yes':
         course_data['Blended'] = 'Yes'
 
+    # SUBJECTS
+    try:
+        THE_XPATH = "//h4[contains(text(), 'Year')]/following::table[1]/tbody/tr/td/div/span/a[@href]"
+        WebDriverWait(browser, 1).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, f'{THE_XPATH}'))
+        )
+        values = browser.find_elements_by_xpath(f'{THE_XPATH}')
+
+        a_tags = set().union(i for i in values)
+        subjects_links = []
+        subject_names = []
+        domain_url = "https://www.griffith.edu.au/"
+        delay = 3
+        for a in a_tags:
+            link = a.get_attribute('href')
+            sub = a.get_attribute('text')
+            if link:
+                link_ = urljoin(domain_url, link)
+                if link_ not in subjects_links:
+                    if link_ not in subjects_links:
+                        subjects_links.append(link_)
+                        subject_names.append(sub)
+            if len(subjects_links) is 40:
+                break
+        i = 1
+        for sl in subjects_links:
+            browser.get(sl)
+            try:
+                THE_XPATH = "//h1[1]"
+                WebDriverWait(browser, delay).until(
+                    EC.presence_of_all_elements_located(
+                        (By.XPATH, f'{THE_XPATH}'))
+                )
+                value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+                course_data[f'Subject_or_Unit_{i}'] = value
+            except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException) as e:
+                print(f'cant extract subject name {i}: {e}')
+            try:
+                THE_XPATH = "//p[@itemprop='description']"
+                WebDriverWait(browser, delay).until(
+                    EC.presence_of_all_elements_located(
+                        (By.XPATH, f'{THE_XPATH}'))
+                )
+                value = browser.find_element_by_xpath(f'{THE_XPATH}').text
+                course_data[f'Subject_Description_{i}'] = value
+            except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException) as e:
+                print(f'cant extract subject description {i}: {e}')
+            except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException) as e:
+                print(f'cant extract subject objective {i}: {e}')
+            print(f"SUBJECT {i}: {course_data[f'Subject_or_Unit_{i}']}\n"
+                  f"SUBJECT OBJECTIVES {i}: {course_data[f'Subject_Objective_{i}']}\n"
+                  f"SUBJECT DESCRIPTION {i}: {course_data[f'Subject_Description_{i}']}\n")
+            i += 1
+    except (AttributeError, TimeoutException, NoSuchElementException, ElementNotInteractableException) as e:
+        print(f'cant extract subjects: {e}')
+
     """
     from selenium.common.exceptions import TimeoutException
     from selenium.webdriver.common.by import By
@@ -406,3 +555,5 @@ with open(csv_file, 'w', encoding='utf-8', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, course_dict_keys)
     dict_writer.writeheader()
     dict_writer.writerows(course_data_all)
+
+browser.quit()
